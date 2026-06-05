d3945acd90d76c0dfd2269c35e31e469965bd8ac
WizzardSK <42868978+WizzardSK@users.noreply.github.com>
2026-06-05 22:34:03 +0200

    CI: add libretro core builds (Linux + Windows/MSVC)
    Linux: distro packages + source-built SDL3/plutovg/plutosvg/rapidyaml/
    libbacktrace (cached via build-deps-linux.sh, also usable locally).
    Windows: upstream's build-dependencies.bat (cached, ffmpeg off) +
    MSVC/Ninja cmake with ENABLE_LIBRETRO=ON. Both upload the core with
    its .info as artifacts.

---
+++ pcsx2-libretro/scripts/build-deps-linux.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+# Builds the dependencies that aren't packaged by Ubuntu into a local prefix
+# (static, PIC), for building the PCEE2 libretro core.
+# Usage: build-deps-linux.sh <install-prefix>
+
+set -e
+
+if [ "$#" -ne 1 ]; then
+	echo "Usage: $0 <install-prefix>"
+	exit 1
+fi
+
+PREFIX=$(realpath "$1")
+NPROCS="$(getconf _NPROCESSORS_ONLN)"
+
+SDL=release-3.4.10
+PLUTOVG=v1.3.2
+PLUTOSVG=v0.0.7
+RAPIDYAML=v0.12.1
+
+mkdir -p deps-build
+cd deps-build
+
+clone() {
+	[ -d "$2" ] || git clone --depth 1 --branch "$3" --recursive "$1" "$2"
+}
+
+clone https://github.com/libsdl-org/SDL sdl3 "$SDL"
+cmake -S sdl3 -B sdl3/build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" -DSDL_SHARED=ON -DSDL_STATIC=OFF \
+	-DSDL_TESTS=OFF -DSDL_EXAMPLES=OFF
+cmake --build sdl3/build --parallel "$NPROCS"
+cmake --install sdl3/build
+
+clone https://github.com/sammycage/plutovg plutovg "$PLUTOVG"
+cmake -S plutovg -B plutovg/build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+	-DBUILD_SHARED_LIBS=OFF -DPLUTOVG_BUILD_EXAMPLES=OFF
+cmake --build plutovg/build --parallel "$NPROCS"
+cmake --install plutovg/build
+
+clone https://github.com/sammycage/plutosvg plutosvg "$PLUTOSVG"
+cmake -S plutosvg -B plutosvg/build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_PREFIX_PATH="$PREFIX" \
+	-DCMAKE_POSITION_INDEPENDENT_CODE=ON -DBUILD_SHARED_LIBS=OFF \
+	-DPLUTOSVG_ENABLE_FREETYPE=ON -DPLUTOSVG_BUILD_EXAMPLES=OFF
+cmake --build plutosvg/build --parallel "$NPROCS"
+cmake --install plutosvg/build
+
+clone https://github.com/biojppm/rapidyaml rapidyaml "$RAPIDYAML"
+cmake -S rapidyaml -B rapidyaml/build -G Ninja -DCMAKE_BUILD_TYPE=Release \
+	-DCMAKE_INSTALL_PREFIX="$PREFIX" -DCMAKE_POSITION_INDEPENDENT_CODE=ON \
+	-DBUILD_SHARED_LIBS=OFF
+cmake --build rapidyaml/build --parallel "$NPROCS"
+cmake --install rapidyaml/build
+
+clone https://github.com/ianlancetaylor/libbacktrace libbacktrace master
+(cd libbacktrace && ./configure --prefix="$PREFIX" --with-pic && make -j"$NPROCS" && make install)
+
+echo "Dependencies installed to $PREFIX"
